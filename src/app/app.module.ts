92bf03ad6f0cec8166718a9535ebb5ebe26cc6fe
John <13274598721@163.com>
2019-09-03 18:28:24 +0800

    stash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDashboardComponent } from './hero-dashboard/hero-dashboard.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HomePageComponent,
+    HeroDetailComponent,
+    HeroDashboardComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomePageComponent },
+      { path: 'heroes', component: HeroesComponent },
+      { path: 'heroes/hero-list', component: HeroListComponent },
+      { path: 'heroes/dashboard', component: HeroDashboardComponent },
+      { path: 'heroes/:heroId', component: HeroDetailComponent }
+    ]),
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
